17b71237d80f3b40f02b79bcf7b8147fe0dcd912
KoehlerT <tim.koehler.net@gmail.com>
2017-12-02 15:54:01 +0100

    Auflösung blumenbilder verändert

---
+++ Basis/Blumenerkennung/ErkennungCis/ErkennungCis/Helper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErkennungCis
+{
+    struct erkannt
+    {
+        public int x, y;
+        public float blumigkeit;
+        public int r;
+        public erkannt(int x, int y, int r, float b)
+        {
+            this.x = x; this.y = y; this.r = r; this.blumigkeit = b;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("An P({0}|{1}) bl={2:0.00} mit r={3}", x, y, blumigkeit, r);
+        }
+    }
+    class Helper
+    {
+    }
+}
