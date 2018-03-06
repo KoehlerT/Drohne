78b062fee3f641aa5d55a4189d681aef175b79a6
KoehlerT <tim.koehler.net@gmail.com>
2018-03-06 18:05:46 +0100

    Implementierung SPI

--- Drohne/Steuerung/src/hardware/SPIManager.java
+++ Drohne/Steuerung/src/hardware/SPIManager.java
@@ -1,5 +1,8 @@
 package hardware;
+import java.io.IOException;
+
 //import jdk.dio.spibus.SPIDeviceConfig;
+import main.Communicator;
 class SPIManager {
 	/*Code abgeschrieben von:
 	 * https://docs.oracle.com/javame/8.0/me-dev-guide/spi.htm
@@ -8,13 +11,12 @@ class SPIManager {
 	//SPIDeviceConfig arduino;
 	
 	public SPIManager() {
-		//https://blogs.oracle.com/javatraining/using-device-io-with-java-embedded-suite-on-a-raspberry-pi
-		/*SPIDeviceConfig config = new SPIDeviceConfig(0, 0,
-                SPIDeviceConfig.CS_ACTIVE_LOW,
-                500000,
-                3,  
-                8,
-                Peripheral.BIG_ENDIAN);*/
+		try {
+			Communicator c = new Communicator(10,4,200000);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
