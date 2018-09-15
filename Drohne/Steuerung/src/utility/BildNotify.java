7d35c039a4100f53fb4eb5cadf2f9075e5ffea30
xPhysalis <hansbata2000@gmail.com>
2018-09-15 21:10:16 +0200

    3D-Pläne für Drucksensorcase

--- Drohne/Steuerung/src/utility/BildNotify.java
+++
@@ -1,8 +0,0 @@
-package utility;
-
-public interface BildNotify {
-	/*Interface für Klassen, die bei neuen Bildern informiert werden wollen.
-	 * Sie brauchen nur eine Methode, die aufgerufen wird, wenn ein neues Bild entstanden ist
-	 * */
-	public void neuesBild(); 
-}
